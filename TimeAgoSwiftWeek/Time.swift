2b3dbff52a1f380a707e9334c3b4db13df8ee7cd
Brage Staven <bragestaven@gmail.com>
2016-01-05 18:26:55 +0100

    added class

---
+++ TimeAgoSwiftWeek/Time.swift
@@ -0,0 +1,52 @@
+//
+//  Time.swift
+//  HuGo
+//
+//  Created by Brage Staven on 21.12.2015.
+//  Copyright © 2015 Stav1. All rights reserved.
+//
+
+import UIKit
+
+
+class Time{
+    class func getTime(fromDate:NSDate!,toDate:NSDate!)-> String{
+
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd-HH-mm"
+        
+        let startDateString = dateFormatter.stringFromDate(fromDate)
+        let endDateString = dateFormatter.stringFromDate(toDate)
+        let startDate:NSDate = dateFormatter.dateFromString(startDateString)!
+        let endDate:NSDate = dateFormatter.dateFromString(endDateString)!
+        
+        let cal = NSCalendar.currentCalendar()
+        let unit:NSCalendarUnit = NSCalendarUnit.Day
+        let components = cal.components(unit, fromDate: endDate, toDate: startDate, options: [])
+        let days = components.day
+        
+        if days >= 7{
+            let unit:NSCalendarUnit = NSCalendarUnit.WeekOfMonth
+            let components = cal.components(unit, fromDate: endDate, toDate: startDate, options: [])
+            return "\(components.weekOfMonth)w"
+        }else if days==0{
+            let unit:NSCalendarUnit = NSCalendarUnit.Hour
+            let components = cal.components(unit, fromDate: endDate, toDate: startDate, options: [])
+            
+            if components.hour==0{
+                let unit2:NSCalendarUnit = NSCalendarUnit.Minute
+                let components2 = cal.components(unit2, fromDate: endDate, toDate: startDate, options: [])
+                print(components2.minute)
+                if components2.minute==0{
+                    return "now"
+                }else{
+                    return "\(components2.minute)m"
+                }
+            }else{
+                return "\(components.hour)h"
+            }
+
+        }
+        return "\(days)d"
+    }
+}
